8cd92e384b7795f20778a91efaac974790779a71
theabsparrow <absbashar04@gmail.com>
2024-09-15 20:43:43 +0600

    login form implemented

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Login></Login>
         </div>
     );
 };
